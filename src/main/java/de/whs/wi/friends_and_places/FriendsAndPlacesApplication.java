3e5a7c2a3f96ca1c1021440f35618710525c5c9b
Dreamonix <fabian.blank01@outlook.com>
2025-06-04 15:48:34 +0200

    build(init): Initial project setup with Spring Initializr

---
+++ src/main/java/de/whs/wi/friends_and_places/FriendsAndPlacesApplication.java
@@ -0,0 +1,13 @@
+package de.whs.wi.friends_and_places;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsAndPlacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsAndPlacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/whs/wi/friends_and_places/FriendsAndPlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.whs.wi.friends_and_places;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsAndPlacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
